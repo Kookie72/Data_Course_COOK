e03506a8c6014e3be3688726093ec286d0fab6fe
Jonia Cook <jonia_too@hotmail.com>
2026-01-22 09:35:25 -0700

    “Assignment_2”

---
+++ Assignments/Assignment_2/Assignment_2_completed.R
@@ -0,0 +1,22 @@
+#Returns a list of all .csv files in specified folder
+list.files(path = "Data", pattern = ".csv")
+#Save a variable that contains a list of all .csv files in specified folder
+csv_files = list.files(path = "Data", pattern = ".csv")
+#Returns to total number of files in variable
+length(csv_files)
+#Creates a variable that contains the contents of specified file
+df = read.csv("Data/wingspan_vs_mass.csv")
+#Returns the first 5 lines of variable
+head(df, n = 5)
+#Find all files in specified directory and subdirectories that start with b
+list.files(path = "Data", pattern = "^b", recursive = TRUE)
+#Displays first line of each file in folder and subfolders that start with b
+b_files = list.files(path = "Data", pattern = "^b", recursive = TRUE, full.names = TRUE)
+for (x in b_files) {
+  print(readLines(x, n = 1))
+}
+#Displays first line of each file in folder and subfolders that end with .csv
+read_csv_files = list.files(path = "Data", pattern = ".csv", recursive = TRUE, full.names = TRUE)
+for (x in read_csv_files) {
+  print(readLines(x, n = 1))
+}
