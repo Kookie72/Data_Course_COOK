be882d562c5f4b1a3c3ee1767e8b3993df872ed8
Jonia Cook <jonia_too@hotmail.com>
2026-02-10 09:56:37 -0700

    “Assignment_3”

--- Assignments/Assignment_3/Assignment_3.R
+++ Assignments/Assignment_3/Assignment_3.R
@@ -13,16 +13,11 @@
 
 # Then, make sure to upload this to both Canvas and your GitHub repository
 
-
-
-
 # Vector operations! ####
 
 # Vectors are 1-dimensional series of values in some order
 1:10 # ':' only works for integers
 letters # built-in pre-made vector of a - z
-
-
 
 vector1 <- c(1,2,3,4,5,6,7,8,9,10)
 vector2 <- c(5,6,7,8,4,3,2,1,3,10)
@@ -34,7 +29,6 @@ vector1*vector2
 
 vector3 + 1 # can't add 1 to "a"
 
-
 # Logical expressions (pay attention to these...they are used ALL THE TIME)
 vector1 > 3
 vector1 >= 3
@@ -45,7 +39,6 @@ letters == "a"
 letters != "c"
 letters %in% c("a","b","c","z")
 vector1 %in% 1:6
-
 
 # Data Frames ####
 # R has quite a few built-in data sets
@@ -59,14 +52,12 @@ data("iris") # load it like this
 # Rows and columns
 # Each row or column is a vector
 
-
 dat <- iris # can rename the object to be easier to type if you want
 
 # ways to get a peek at our data set
 names(dat)
 dim(dat)
 head(dat)
-
 
 # You can access specific columns of a "data frame" by name using '$'
 dat$Species
@@ -79,7 +70,6 @@ dat[1:3,5]
 vector2[1]
 letters[1:7]
 letters[c(1,3,5,7)]
-
 
 # Plotting ####
 
@@ -110,7 +100,6 @@ as.factor(nums) # show in console
 nums_factor <- as.factor(nums) #assign it to a new object as a factor
 class(nums_factor) # check it
 
-
 # convert numeric to character
 as.character(vector1)
 as.character(vector1) + 5
@@ -118,16 +107,10 @@ as.character(vector1) + 5
 # convert character to numeric
 as.numeric(vector3)
 
-
-
-
 #check it out
 plot(nums) 
 plot(nums_factor)
 # take note of how numeric vectors and factors behave differently in plot()
-
-
-
 
 # Simple numeric functions
 # R is a language built for data analysis and statistics so there is a lot of functionality built-in
@@ -148,35 +131,49 @@ cummax(vector1)
 # even has built-in statistical distributions (we will see more of these later)
 dbinom(50,100,.5) # probability of getting exactly 50 heads out of 100 coin flips
 
-
-
-
 # YOUR REMAINING HOMEWORK ASSIGNMENT (Fill in with code) ####
 
-# 1.  Get a subset of the "iris" data frame where it's just even-numbered rows
+# 1.  Get a subset of the "iris" data frame where it's just even-numbered rows####
+df_even = iris
+df_even = df_even[seq(2, nrow(df_even), by = 2), ]
+View(df_even)
+#Using tidyverse
+library(tidyverse)
+df_even_tidy = iris
+df_even_tidy = df_even_tidy %>% 
+  slice(seq(2, n(), by = 2))
+View(df_even_tidy)
 
 seq(2,150,2) # here's the code to get a list of the even numbers between 2 and 150
 
+# 2.  Create a new object called iris_chr which is a copy of iris, except where every column is a character class####
+library(tidyverse)
+iris_chr = iris %>% 
+  mutate(across(everything(), as.character))
+str(iris_chr)
+         
+# 3.  Create a new numeric vector object named "Sepal.Area" which is the product of Sepal.Length and Sepal.Width####
+library(tidyverse)
+Sepal.Area = iris$Sepal.Length * iris$Sepal.Width
+view(iris)
+view(Sepal.Area)
+str(Sepal.Area)
 
+# 4.  Add Sepal.Area to the iris data frame as a new column####
+library(tidyverse)
+new_iris = iris %>% 
+  mutate(Sepal.Area = iris$Sepal.Length * iris$Sepal.Width)
+view(new_iris)
 
-# 2.  Create a new object called iris_chr which is a copy of iris, except where every column is a character class
-
-
-
-# 3.  Create a new numeric vector object named "Sepal.Area" which is the product of Sepal.Length and Sepal.Width
-
-
-
-# 4.  Add Sepal.Area to the iris data frame as a new column
-
-
-
-# 5.  Create a new dataframe that is a subset of iris using only rows where Sepal.Area is greater than 20 
+# 5.  Create a new dataframe that is a subset of iris using only rows where Sepal.Area is greater than 20 ####
       # (name it big_area_iris)
+library(tidyverse)
+big_area_iris = iris %>% 
+  mutate(Sepal.Area = iris$Sepal.Length * iris$Sepal.Width) %>% 
+  filter(Sepal.Area > 20)
+view(big_area_iris)
 
-
-
-# 6.  Upload the last numbered section of this R script (with all answers filled in and tasks completed) 
+# 6.  Upload the last numbered section of this R script (with all answers filled in and tasks completed) ####
       # to canvas
       # I should be able to run your R script and get all the right objects generated
 
